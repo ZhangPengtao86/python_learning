512724deb7c53ef6eae7fd5c0721c049821cc7bc
ZhangPengtao86 <867036985@qq.com>
2017-11-30 20:02:45 +0800

    Create print_practice.py

---
+++ scripts_dir/print_practice.py
@@ -0,0 +1,10 @@
+>>> a='how'
+>>> b='old'
+>>> c='are'
+>>> d='you'
+>>> print '%s...%s...%s...%s' %( a,b,c,d)
+how...old...are...you
+
+
+>>> print'{}...{}...{}...{}'.format(a,b,c,d)
+how...old...are...you
